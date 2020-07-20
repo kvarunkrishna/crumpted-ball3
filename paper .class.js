449290dc68cbc84574b3c3ad2b8fb13b2a22f911
kvarunkrishna <66196037+kvarunkrishna@users.noreply.github.com>
2020-07-20 00:25:03 -0500

    Add files via upload

---
+++ paper .class.js
@@ -0,0 +1,8 @@
+class BaseClass{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            isstatic=false,
+            'restitution':0.3,
+            'friction':1.5,
+            'density':1.2
+        }
